da2363ba3239f65aac4aa2bd2cdbc3ee1a849db6
Elias Castro <elias.scastro@gmail.com>
2020-11-02 10:31:37 -0300

    Adicionando classes web model.

---
+++ src/main/java/br/com/castro/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,8 @@
+package br.com.castro.msscbeerservice.web.model;
+
+/**
+ * @Author Elias Castro em 02/11/2020
+ */
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
